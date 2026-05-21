7a6eb319cd7c29881a980dcb62aae3fe0b63380c
Khush81699 <khushboosgupta864@gmail.com>
2026-05-21 23:56:04 +0530

    Added Positive Negative Zero and Even Odd Programs

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,39 @@ public class Main {
     public static void main(String[]args)
     {
         System.out.println("Hello Java\uD83D\uDE80");
+
+        // Question 1:
+        // Check Positive, Negative, or Zero
+
+        int number = -6;
+
+        if(number > 0)
+        {
+            System.out.println("Positive");
+        }
+
+        else if(number < 0)
+        {
+            System.out.println("Negative");
+        }
+
+        else {
+            System.out.println("Zero");
+        }
+
+            // Question 2:
+// Check Even or Odd
+
+            int num = 5;
+
+            if(num % 2 == 0)
+            {
+                System.out.println("Even");
+            }
+            else
+            {
+                System.out.println("Odd");
+            }
+        }
+
     }
-}
