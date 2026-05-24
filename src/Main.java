24c4bc42eee4c38d6a3080dc9aff22eb124aa988
Khush81699 <khushboosgupta864@gmail.com>
2026-05-24 22:53:11 +0530

    Added Java questions 11 to 15

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         System.out.println("Hello Java\uD83D\uDE80");
 
         // Question 1:
@@ -69,13 +69,9 @@ public class Main {
 
         int year = 2024;
 
-        if(year % 4 == 0)
-        {
+        if (year % 4 == 0) {
             System.out.println("Leap Year");
-        }
-
-        else
-        {
+        } else {
             System.out.println("Not Leap Year");
         }
 
@@ -85,13 +81,9 @@ public class Main {
 
         char ch = 'A';
 
-        if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
-        {
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
             System.out.println("Alphabet");
-        }
-
-        else
-        {
+        } else {
             System.out.println("Not Alphabet");
         }
 
@@ -101,13 +93,9 @@ public class Main {
 
         char letter = 'e';
 
-        if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
-        {
+        if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
             System.out.println("Vowel");
-        }
-
-        else
-        {
+        } else {
             System.out.println("Consonant");
         }
 
@@ -117,13 +105,9 @@ public class Main {
 
         int n2 = 55;
 
-        if(n2 % 5 == 0 || n2 % 11 == 0)
-        {
+        if (n2 % 5 == 0 || n2 % 11 == 0) {
             System.out.println("Divisible");
-        }
-
-        else
-        {
+        } else {
             System.out.println("Not Divisible");
         }
 
@@ -137,6 +121,180 @@ public class Main {
         int max = (p > q) ? p : q;
 
         System.out.println("Largest = " + max);
+
+
+        // Question 11:
+        // Check Whether Character is Uppercase or Lowercase
+
+        char ch1 = 'A';
+
+        if(ch1 >= 'A' && ch1 <= 'Z')
+        {
+            System.out.println("Uppercase");
+        }
+
+        else
+        {
+            System.out.println("Lowercase");
+        }
+
+
+
+
+        // Question 12:
+        // Check Whether Number is Three Digit or Not
+
+        int num1 = 456;
+
+        if(num1 >= 100 && num1 <= 999)
+        {
+            System.out.println("Three Digit Number");
+        }
+
+        else
+        {
+            System.out.println("Not Three Digit");
+        }
+
+
+
+
+        // Question 13:
+        // Check Whether Number is Multiple of 7
+
+        int num2 = 21;
+
+        if(num2 % 7 == 0)
+        {
+            System.out.println("Multiple of 7");
+        }
+
+        else
+        {
+            System.out.println("Not Multiple of 7");
+        }
+
+
+
+
+        // Question 14:
+        // Check Eligibility for Voting
+
+        int age = 20;
+
+        if(age >= 18)
+        {
+            System.out.println("Eligible for Voting");
+        }
+
+        else
+        {
+            System.out.println("Not Eligible");
+        }
+
+
+
+
+        // Question 15:
+        // Check Whether Character is Digit or Alphabet
+
+        char ch2 = '5';
+
+        if(ch2 >= '0' && ch2 <= '9')
+        {
+            System.out.println("Digit");
+        }
+
+        else
+        {
+            System.out.println("Alphabet");
+        }
+
+
+
+
+        // Question 16:
+        // Greater Between Two Numbers Using Ternary Operator
+
+        int m1 = 50;
+        int n1 = 80;
+
+        int greater = (m1 > n1) ? m1 : n1;
+
+        System.out.println("Greater Number = " + greater);
+
+
+
+
+        // Question 17:
+        // Smaller Between Two Numbers
+
+        int p1 = 25;
+        int q1 = 15;
+
+        if(p1 < q1)
+        {
+            System.out.println("P is Smaller");
+        }
+
+        else
+        {
+            System.out.println("Q is Smaller");
+        }
+
+
+
+
+        // Question 18:
+        // Check Whether Number is Positive Even Number
+
+        int num3 = 8;
+
+        if(num3 > 0 && num3 % 2 == 0)
+        {
+            System.out.println("Positive Even Number");
+        }
+
+        else
+        {
+            System.out.println("Not Positive Even");
+        }
+
+
+
+
+        // Question 19:
+        // Check Whether Number is Negative Odd Number
+
+        int num4 = -9;
+
+        if(num4 < 0 && num4 % 2 != 0)
+        {
+            System.out.println("Negative Odd Number");
+        }
+
+        else
+        {
+            System.out.println("Not Negative Odd");
+        }
+
+
+
+
+        // Question 20:
+        // Check Whether Temperature is Hot or Cold
+
+        int temp = 35;
+
+        if(temp >= 30)
+        {
+            System.out.println("Hot");
+        }
+
+        else
+        {
+            System.out.println("Cold");
+        }
+
     }
 }
-
